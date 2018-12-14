bcb91a1ee573c133acd44972795acf94bb0ec4ef
andrey <andrey@osmushko.com>
2018-12-14 14:24:19 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import HelloWorld from './components/hello-world';
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+              <li>
+                <Link to="/hello/">Hello</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+          <Route path="/hello/" component={HelloWorld} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ src/components/hello-world/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import style from './hello-world.css';
+
+// const HelloWorld = ({ title }) => (
+//   <div className={style['hello-world']}>{title}</div>
+// );
+//
+// HelloWorld.propTypes = {
+//   title: PropTypes.string,
+// };
+
+class HelloWorld extends Component {
+  render() {
+    return <div className={style['hello-world']}>TEST</div>;
+  }
+}
+
+export default HelloWorld;
